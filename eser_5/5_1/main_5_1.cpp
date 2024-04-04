ec4ca196abcbf7bb39112e7565bf4d79edfe8863
RandDouble <stefano.pilosio@gmail.com>
2024-04-04 09:43:36 +0200

    prepared for exercise 5

---
+++ eser_5/5_1/main_5_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+
+#include <vector>
+
+#include "metropolis.h"
+#include "utilities.h"
+#include "wave_function.h"
+
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
---
+++ eser_5/5_1/wave_function.h
@@ -0,0 +1,7 @@
+#ifndef __WAVE_FUNCTION__
+#define __WAVE_FUNCTION__
+
+#include <numeric>
+
+
+#endif // __WAVE_FUNCTION__
