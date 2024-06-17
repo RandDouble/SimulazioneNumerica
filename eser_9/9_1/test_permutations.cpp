de02da2f30f63cf4581054a743de7737897efb5a
RandDouble <stefano.pilosio@gmail.com>
2024-06-17 18:18:28 +0200

    Testing new algorithms, including it in makefile

--- eser_9/9_1/test_permutations.cpp
+++ eser_9/9_1/test_permutations.cpp
@@ -6,9 +6,70 @@
 #include "initializer.h"
 #include "mutation.h"
 #include "random.h"
+#include "swap_functions.h"
 #include "utilities.h"
 
 constexpr std::size_t SIZE = 10;
+
+template <std::size_t SIZE = SIZE>
+bool test_swap_functions_swap_ranges()
+{
+    std::array<int, SIZE> test_arr;
+    bool is_correct = true;
+    print_vector<int> print;
+    for (std::size_t i = 0; i < SIZE; i++)
+    {
+        std::iota(test_arr.begin(), test_arr.end(), 0);
+
+        PBC_swap::swap_ranges<SIZE>(test_arr.begin(), i, i + 4, 3, 1);
+        print(test_arr);
+        // Checking if the first is untouched
+        is_correct = is_correct && (test_arr[0] == 0);
+
+        assert(is_correct && "Modified first element, something wrong");
+    }
+    return is_correct;
+}
+
+template <std::size_t SIZE = SIZE>
+bool test_swap_functions_rotate()
+{
+    std::array<int, SIZE> test_arr;
+    bool is_correct = true;
+    print_vector<int> print;
+    for (std::size_t i = 0; i < SIZE; i++)
+    {
+        std::iota(test_arr.begin(), test_arr.end(), 0);
+
+        PBC_swap::rotate<SIZE>(test_arr.begin(), i, i + 3, i + 5, 1);
+        print(test_arr);
+        // Checking if the first is untouched
+        is_correct = is_correct && (test_arr[0] == 0);
+
+        assert(is_correct && "Modified first element, something wrong");
+    }
+    return is_correct;
+}
+
+template <std::size_t SIZE = SIZE>
+bool test_swap_functions_reverse()
+{
+    std::array<int, SIZE> test_arr;
+    bool is_correct = true;
+    print_vector<int> print;
+    for (std::size_t i = 0; i < SIZE; i++)
+    {
+        std::iota(test_arr.begin(), test_arr.end(), 0);
+
+        PBC_swap::reverse<SIZE>(test_arr.begin(), i, i + SIZE - 1, 1);
+        print(test_arr);
+        // Checking if the first is untouched
+        is_correct = is_correct && (test_arr[0] == 0);
+
+        assert(is_correct && "Modified first element, something wrong");
+    }
+    return is_correct;
+}
 
 template <std::size_t SIZE = SIZE>
 bool test_PBC()
@@ -91,12 +152,12 @@ bool test_shift_block()
 
     Individual<SIZE> cities;
 
-    std::cout << "Positions Before Pair Permutation :\n";
+    std::cout << "Positions Before Shift Block :\n";
     cities.print_DNA();
 
     cities.shift_block(rng);
 
-    std::cout << "Positions After Pair Permutation :\n";
+    std::cout << "Positions After Shift Block :\n";
 
     cities.print_DNA();
 
@@ -105,6 +166,7 @@ bool test_shift_block()
 
     for (int i = 0; i < 100; i++)
     {
+        // std::cout << i << '\n';
         cities.shift_block(rng);
     }
 
@@ -125,6 +187,7 @@ bool test_shift_block()
 template <std::size_t SIZE = SIZE>
 bool test_permutate_contiguos()
 {
+
     bool is_correct = true;
     Random rng;
     initializer(rng);
@@ -210,8 +273,8 @@ bool test_crossover()
     Random rng;
     initializer(rng);
 
-    Individual<SIZE> father={0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    Individual<SIZE> mother={0, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    Individual<SIZE> father = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    Individual<SIZE> mother = {0, 9, 8, 7, 6, 5, 4, 3, 2, 1};
     Individual<SIZE> son, daughter;
 
     bool is_correct = true;
@@ -232,22 +295,22 @@ bool test_crossover()
 
     // Checking if the first is untouched
     std::cout << "Checking Father\n";
-    is_correct = is_correct && father.check();
+    is_correct = is_correct && father.check_health();
     if (!is_correct)
         std::cout << "Father is BAD\n";
 
     std::cout << "Checking Mother\n";
-    is_correct = is_correct && mother.check();
+    is_correct = is_correct && mother.check_health();
     if (!is_correct)
         std::cout << "Mother is BAD\n";
 
     std::cout << "Checking Son\n";
-    is_correct = is_correct && son.check();
+    is_correct = is_correct && son.check_health();
     if (!is_correct)
         std::cout << "Son is BAD\n";
 
     std::cout << "Checking Daughter\n";
-    is_correct = is_correct && daughter.check();
+    is_correct = is_correct && daughter.check_health();
     if (!is_correct)
         std::cout << "Daughter is BAD\n";
 
@@ -256,6 +319,38 @@ bool test_crossover()
 
 int main()
 {
+#ifdef NDEBUG
+    std::cout << "Active NDEBUG, test results may not be complete\n";
+#endif
+    if (test_swap_functions_swap_ranges())
+    {
+        std::cout << "SWAP RANGES TEST : PASSED\n\n";
+    }
+    else
+    {
+        std::cout << "SWAP RANGES TEST : FAILED : ERROR\n\n";
+        exit(-1);
+    }
+
+    if (test_swap_functions_rotate())
+    {
+        std::cout << "ROTATE TEST : PASSED\n\n";
+    }
+    else
+    {
+        std::cout << "ROTATE TEST : FAILED : ERROR\n\n";
+        exit(-1);
+    }
+
+    if (test_swap_functions_reverse())
+    {
+        std::cout << "REVERSE TEST : PASSED\n\n";
+    }
+    else
+    {
+        std::cout << "REVERSE TEST : FAILED : ERROR\n\n";
+        exit(-1);
+    }
 
     if (test_PBC())
     {
@@ -263,7 +358,7 @@ int main()
     }
     else
     {
-        std::cout << "PBC TEST : FAILED : ERROR\n";
+        std::cout << "PBC TEST : FAILED : ERROR\n\n";
         exit(-1);
     }
 
@@ -273,47 +368,47 @@ int main()
     }
     else
     {
-        std::cout << "Pair Permutation TEST : FAILED : ERROR\n";
+        std::cout << "Pair Permutation TEST : FAILED : ERROR\n\n";
         exit(-1);
     }
 
     if (test_permutate_contiguos())
     {
-        std::cout << "Permutate Contiguos TEST : PASSED\n";
+        std::cout << "Permutate Contiguos TEST : PASSED\n\n";
     }
     else
     {
-        std::cout << "Permutate Contiguos TEST : FAILED : ERROR\n";
+        std::cout << "Permutate Contiguos TEST : FAILED : ERROR\n\n";
         exit(-1);
     }
 
     if (test_shift_block())
     {
-        std::cout << "Shift Block TEST : PASSED\n";
+        std::cout << "Shift Block TEST : PASSED\n\n";
     }
     else
     {
-        std::cout << "Shift Block TEST : FAILED : ERROR\n";
+        std::cout << "Shift Block TEST : FAILED : ERROR\n\n";
         exit(-1);
     }
 
     if (test_inversion())
     {
-        std::cout << "Inversion TEST : PASSED\n";
+        std::cout << "Inversion TEST : PASSED\n\n";
     }
     else
     {
-        std::cout << "Inversion TEST : FAILED : ERROR\n";
+        std::cout << "Inversion TEST : FAILED : ERROR\n\n";
         exit(-1);
     }
 
     if (test_crossover())
     {
-        std::cout << "Crossover TEST : PASSED\n";
+        std::cout << "Crossover TEST : PASSED\n\n";
     }
     else
     {
-        std::cout << "Crossover TEST : FAILED : ERROR\n";
+        std::cout << "Crossover TEST : FAILED : ERROR\n\n";
         exit(-1);
     }
 
