028e33e4e7e7fb04050e540892bf569c462e1c95
RandDouble <stefano.pilosio@gmail.com>
2024-08-21 22:06:45 +0200

    Started Exercise 7 end

--- eser_7/autocorrelate.py
+++ eser_7/autocorrelate.py
@@ -1,13 +1,71 @@
-import  matplotlib.pyplot as plt
+import argparse
+
+import matplotlib.pyplot as plt
 import numpy as np
-import scipy.signal as sgn
+import pandas as pd
+import scipy.optimize as optimize
+import scipy.signal as signal
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
 
 
-def print_image():
-    plt.
+def autocorrelation(data: np.ndarray) -> np.ndarray:
+    mean = data.mean()
+    v_autocorrelation = signal.correlate(data - mean, data - mean, mode="same")
+    v_autocorrelation /= v_autocorrelation.max()
+    return v_autocorrelation
+
+
+def exp_func(x, tau, a):
+    return a * np.exp(-x / tau)
+
+
+def fit_exp_autocorrelation(data: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    x = np.arange(data[data.size // 2 : -1].size)
+    popt, pcov = optimize.curve_fit(exp_func, x, data[data.size // 2 : -1])
+    return popt, pcov
+
+
+def print_autocorr(autocorr: np.ndarray, popt: np.ndarray) -> tuple[Figure, Axes]:
+    fig: Figure
+    ax: Axes
+    fig, ax = plt.subplots()
+    time_step = np.arange(autocorr[autocorr.size // 2 : -1].size)
+    ax.plot(time_step, autocorr[autocorr.size // 2 : -1], label="Autocorrelation")
+    ax.plot(
+        time_step,
+        exp_func(time_step, *popt),
+        label="Fitted curve," + r" $\tau$ :" + f" {popt[0]:.2f}",
+    )
+    ax.set_ylabel("Autocorrelation")
+    ax.set_xlabel("steps")
+    ax.set_title("Autocorrelation vs time steps")
+    return fig, ax
+
 
 def main():
-    print_image()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "file", required=True, help="Name of the file to be analyzed, can be only csv"
+    )
+    parser.add_argument(
+        "idx", required=False, default=1, help="Column index for autocorrelation"
+    )
+    args = parser.parse_args()
+    file = args.file
+    conv_idx = args.idx
 
-if __name__ == '__main__':
+    df = pd.read_csv(file)
+
+    data = df.iloc[:, conv_idx].to_numpy()
+
+    autocorr = autocorrelation(data)
+    popt, _ = fit_exp_autocorrelation(autocorr)
+
+    fig: Figure
+    fig, _ = print_autocorr(autocorr, popt)
+    fig.savefig("autocorrelation.svg")
+
+
+if __name__ == "__main__":
     main()
